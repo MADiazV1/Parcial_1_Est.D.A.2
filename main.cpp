d477f8fa3941f63b88f1ece1bea64fbbb89f6510
Madiazv1 <diazvalenciam9@gmail.com>
2023-03-01 19:26:17 -0500

    Inicio repositorio

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void creacion_nombre_completo(int cantidadNombre){
+    string nombres[20] = {"Bruno","Dante","Hugo","Miguel","Samuel","Jose","Daniel","Grabriel","Sofia","Santiago","Julian","Mariana","Valeria","Ana","Valentina","Daniela","Laura","Tomas","Jacobo","Sebastian"};
+
+    string apellido[20] = {"Diaz","Ramirez","Aguilar","Gomez","Valencia","Villada","Gonzales","Zamudio","Restrepo","Escobar","Rodriguez","Moreno","Palacio","Chaverra","Ruiz","Barrera","Giraldo","Gutierrez","Salazar","Betancur"};
+
+    for(int i=0;i<cantidadNombre;i++){
+        
+        string nombre = nombre[x] + " " + apellido[y] + " " + apellido[z];
+    }
+}
+
+int main(){
+
+    return 0;
+}
